63cf8b110b3bad16ec1affd42beb1cc67bbdf80e
amantripathigithub <at425020@gmail.com>
2023-06-23 16:28:14 +0530

    map is added

--- user/src/components/Helpme.js
+++ user/src/components/Helpme.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { useState } from 'react';
-
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW1hbnRyaXBhdGhpNiIsImEiOiJjbGo4Y3NoNjYxOWlvM2Z0ZWlqeDdtcG83In0.4lpEdOMCUUfO9xFQJzk86g';
+ 
 export default function Helpme() {
-
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(0);
   const [lat, setLat] = useState(0);
-  const [lon, setLon] = useState(0);
-
+  const [zoom, setZoom] = useState(13);
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(success, error);
   } else {
@@ -15,8 +18,8 @@ export default function Helpme() {
   function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
+    setLng(longitude);
     setLat(latitude);
-    setLon(longitude)
     console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
   }
   
@@ -27,15 +30,32 @@ export default function Helpme() {
 
 
 
-
-
-
-
-  return (
-    <div>
-      {lat}
-      <br></br>
-      {lon}
-    </div>
-  )
+ 
+useEffect(() => {
+// if (map.current) return; // initialize map only once
+map.current = new mapboxgl.Map({
+container: mapContainer.current,
+style: 'mapbox://styles/mapbox/streets-v12',
+center: [lng, lat],
+zoom: zoom
+});
+});
+ 
+useEffect(() => {
+// if (!map.current) return; // wait for map to initialize
+map.current.on('move', () => {
+setLng(map.current.getCenter().lng.toFixed(4));
+setLat(map.current.getCenter().lat.toFixed(4));
+setZoom(map.current.getZoom().toFixed(2));
+});
+});
+ 
+return (
+<div>
+<div className="sidebar">
+Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+</div>
+<div ref={mapContainer} className="map-container" />
+</div>
+);
 }
--- user/src/index.js
+++ user/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
